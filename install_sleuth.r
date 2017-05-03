b3f70319a676896f634b7cbfec4c13bcd0d8e758
Hussin Group <hussingroup@JulieHusnewMBP2.statgen.local>
2017-05-03 13:16:59 -0400

    add install_sleuth.r

--- install_sleuth.r
+++ install_sleuth.r
@@ -3,3 +3,9 @@ biocLite("rhdf5")
 biocLite("biomaRt")
 install.packages("devtools")
 devtools::install_github("pachterlab/sleuth")
+
+
+source("http://bioconductor.org/biocLite.R") # This allows you to install Bioconductor packages
+biocLite("seqplots")
+library(seqplots)
+run()
